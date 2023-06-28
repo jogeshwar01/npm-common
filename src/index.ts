08b438617150856bd095a432bc6159ae885d4b36
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2023-06-28 22:58:14 +0530

    Updates

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,19 @@ export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
+
+// COMMAND TO BE RUN
+// 1 - npm login
+// 2 - npm publish --access public
+// 3 - npm run pub - set up in package.json
+//   - build folder contains the bundled js files from ts
+//   - do 'tsc --init' for tsconfig.json
+//     npm install @js-ticketly/common  - to install in other services
+
+// if we change something here and we want it to be reflected in the service its used
+// first in common directory do npm run pub
+// then go in the service using this - npm update @js-ticketly/common
+
+// can check if container is running correct version
+// can do exec -it into it and check the json file as well
+// this is not needed, just informational
